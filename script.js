434fc7558b399c45da6d346582ca8958de455e9b
meni.tasa <menit@outlook.co.il>
2022-11-11 08:01:03 +0200

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText  = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 let apiQuotes = [];
 
 //Show New Quote
 const newQuote = () => {
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)] 
-  console.log(quote)
-}
-
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  }else {
+    authorText.textContent = quote.author
+  }
+  
+  if (quote.text.length > 50){
+   quoteText.classList.add(('long-quote'))
+  } else  {
+    quoteText.classList.remove(('long-quote'))
+  }
+    quoteText.textContent = quote.text
+  
+};
 
 // Get Quotes From API
 async function getQuotes() {
@@ -19,6 +38,17 @@ async function getQuotes() {
     alert(error);
   }
 }
+
+
+//Tweet Quote
+function tweetQuote(){
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+  window.open(twitterUrl, '_blank')
+}
+
+
+newQuoteBtn.addEventListener('click',newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuotes()
